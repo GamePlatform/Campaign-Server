eb8e6f073a1775775a9aead6bde9815d132d7e1c
donnaknew <donnaknew@gmail.com>
2017-05-25 20:21:33 +0900

    간단한 테스트를 위한 api

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/api/users');
+var campaign = require('./routes/api/campaign');
 
 var app = express();
 
@@ -23,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/api/users', users);
+app.use('/api/campaign', campaign);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
