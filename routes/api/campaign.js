dec26918d06da68cb0a582ced46e530e9b09caa6
mihyun <algus737@gmail.com>
2017-06-04 18:32:08 +0900

    test commit

--- routes/api/campaign.js
+++ routes/api/campaign.js
@@ -10,4 +10,16 @@ router.get('/locations/:locationID', function(req, res, next){
   res.json({'images':[{'url':'http://wallpaper-gallery.net/images/image/image-13.jpg'},{'url':'https://www.w3schools.com/css/img_fjords.jpg'}]});
 });
 
+router.delete('/info', function(req, res, next){
+
+  // request 안에서 campaignID'들'을 가져오기
+    var campIDArray = req.body.campaignID; //배열을 가져오기
+  //db access -> delete infomation using id
+
+  // if 성공
+  // res.send('good!');
+  // else 실패
+  // res.send('fail!');
+});
+
 module.exports = router;
