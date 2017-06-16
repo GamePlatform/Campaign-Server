70738cf99e4df0a2d7249eddc9eafa929a834eab
황원준(Hwang Won Jun) <neodarkness728@gmail.com>
2017-06-16 14:39:21 +0900

    REST API - 광고 등록(POST) (#28)
    * REST API - 광고 등록(POST)
    
    data를 받아 db 접속하여 저장까지 동작.
    
    * 코드 수정
    
    빈공간을 제거하고 콘솔에 출력하는 부분 중 불필요한 것  제거
    
    * function 마지막에 return이 필요없어서 지움

--- routes/api/campaign.js
+++ routes/api/campaign.js
@@ -49,4 +49,29 @@ router.get('/locations/:locationID', function(req, res, next){
   });
 });
 
+router.post('/url', function(req, res, next){
+  let title = req.body.title;
+  let url = req.body.url;
+  let expireDay = req.body.expireDay;
+  let startDate = req.body.startDate;
+  let endDate = req.body.endDate;
+
+  var sql = 'insert into campaign_info '
+  +'(title,url,ad_expire_day,start_date,end_date)'
+  +'values (?,?,?,?,?)';
+  
+  var query = connection.query(sql, [title,url,expireDay,startDate,endDate], function(err, result){
+    if(err){
+      console.error(err);
+      //throw err; //처리가 필요함.
+      res.status(400).send('error'); // 에러처리에 대한 예비코드
+      return;
+    }
+
+    console.log(query.sql);
+    console.log("Number of records inserted: " + result.affectedRows);
+    res.status(200).send('success');
+  });
+});
+
 module.exports = router;
