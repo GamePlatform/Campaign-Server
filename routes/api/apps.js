7e7f4f994a64bede70a84b03735532ec0036cd30
donnaknew <donnaknew@gmail.com>
2017-06-19 03:01:49 +0900

    PUT method, update one app, DELETE method, delete one app

--- routes/api/apps.js
+++ routes/api/apps.js
@@ -18,7 +18,7 @@ router.get('/', function(req, res, next) {
   appQuery = connection.query('select id, title from app_info', function(err, appRows){
     if (err) {
       console.error(err);
-      throw err;
+      res.status(400).send('GET ALL, api/apps/ DB select error.');
     }
     apps = appRows;
 
@@ -27,6 +27,7 @@ router.get('/', function(req, res, next) {
     }else if(apps){
       queryCount = 1;
     }else{
+      //이 부분 에러 처리 할지?
       queryCount = 0;
     }
 
@@ -37,16 +38,54 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res){
-  var app_info = {
-    'title': req.body.app_info[0].title
-  };
+  //논의 사항 - 요청하는 json 방식
+  // var app_info = {
+  //   'title': req.body.app_info[0].title
+  // };
+  var appQuery;
+  var insertApp = req.body.app_info[0].title;
 
-  var query = connection.query('insert into app_info set ?', app_info,function(err,rows){
+  appQuery = connection.query('insert into app_info (title) values (?)', insertApp, function(err, appRows){
     if (err) {
       console.error(err);
-      throw err;
+      res.status(400).json({'error':'POST, api/apps/, DB insert, error'});
     }
-    res.status(200).send('Your app registration was successful.');
+    res.status(200).json({'result':'Your app has been successfully registered.'});
+  });
+});
+
+router.put('/:aid', function(req, res){
+  var appQuery;
+  var aid = req.params.aid;
+  var updateApp = req.body.app_info[0].title;
+
+  appQuery = connection.query('update app_info set title=? where id=?', [updateApp, aid], function(err, appRows){
+    if(err){
+      console.error(err);
+      res.status(400).json({'error':'PUT ONE, api/apps/, DB update, error'});
+    }
+    if(appRows.affectedRows == 0){
+      res.status(400).json({'error':'PUT ONE, api/apps/, DB update, no data'});
+    }else{
+      res.status(200).json({'result':'Your app has been successfully updated.'});
+    }
+  });
+});
+
+router.delete('/:aid', function(req, res){
+  var appQuery;
+  var aid = req.params.aid;
+
+  appQuery = connection.query('delete from app_info where id=?', aid, function(err, appRows){
+    if(err){
+      console.error(err);
+      res.status(400).json({'error':'DELETE ONE, api/apps/, DB delete, error'});
+    }
+    if(appRows.affectedRows == 0){
+      res.status(400).json({'error':'DELETE ONE, api/apps/, DB delete, no data'});
+    }else{
+      res.status(200).json({'result':'Your app has been successfully deleted.'});
+    }
   });
 });
 
@@ -57,15 +96,13 @@ router.get('/:aid', function(req, res, next) {
   appQuery = connection.query('select title from app_info where id=?', aid, function(err, appRows){
     if (err) {
       console.error(err);
-      throw err;
+      res.status(400).json({'error':'GET ONE, api/apps/, DB select, error'});
     }
-
-    //need error
-    // if(!apps){
-    //   throw err;
-    // }
-
-    res.json(appRows[0]);
+    if(appRows == 0){
+      res.status(400).json({'error':'GET ONE, api/apps/, DB select, no data'});
+    }else{
+      res.status(200).json(appRows[0]);
+    }
   });
 });
 
