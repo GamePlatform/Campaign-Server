ade2e597fb6173b2c42b607e832d4a6a8e803013
WJ-PC\WJ <neodarkness728@gmail.com>
2017-06-28 15:12:47 +0900

    [GET, PUT, POST - Campaign infomation]
    app.js
    - File upload module 추가
    - 서버 작동 시에 Upload된 이미지를 저장할 공간인 upload_image 폴더 생성 코드 추가
    
    routes/api/apps.js
    - 기존 campaign.js에 있던 app id와 location id를 이용하여 광고 정보를 받는 get Method 옮김.
    
    routes/api/campaign.js
    - image를 업로드 할 수 있는 post method 구현
    - 광고 정보 전체를 받아오는 get method 구현
    - 지정한 광고의 정보를 변경하는 put method 구현

--- routes/api/campaign.js
+++ routes/api/campaign.js
@@ -10,42 +10,32 @@ var connection = mysql.createConnection({
   database:'campaigndb'
 });
 
-router.get('/:appid/locations/:locationID/campaigns', function(req, res, next){
+router.get('/', function(req, res, next){
 
-// var dId = req.query.did;
-  var aId = req.params.appid;
-  var lId = req.params.locationID;
-  var ecArr = req.query.ec;
+  var sql = 'select * from campaign_info';
 
-  var campaignQuery;
-  var campaigns;
-  var queryCount;
-
-  var campaignsJoinQuery = 'select cl.campaign_id, cl.campaign_order, ci.title, ci.url, ci.ad_expire_day ' +
-          'from campaign_for_location as cl inner join location_for_app as la on cl.location_id=la.location_id ' +
-          'inner join campaign_info as ci on cl.campaign_id=ci.id ' +
-          'where la.app_id=? and la.location_id=? and ci.id not in (?)';
-  campaignQuery = connection.query(campaignsJoinQuery, [aId, lId, ecArr], function(err, camRows){
-    if (err) {
+  campaignQuery = connection.query(sql, null, function(err, rows){
+    if(err){
       console.error(err);
-      res.status(400).send('GET Campaigns, DB select error.');
+      //throw err; //처리가 필요함.
+      res.status(400).send('error'); // 에러처리에 대한 예비코드
+      return;
     }
-    campaigns = camRows;
 
-    if(Array.isArray(campaigns)){
-      queryCount = campaigns.length;
-    }else if(campaigns){
+    if(Array.isArray(rows)){
+      queryCount = rows.length;
+    }else if(rows){
       queryCount = 1;
     }else{
       queryCount = 0;
     }
 
-    console.log(campaigns);
+    console.log(rows);
     console.log(queryCount);
 
     res.json({'count':queryCount,
-                campaigns
-            });
+      'campaigns': rows
+    });
   });
 });
 
@@ -61,6 +51,101 @@ router.post('/url', function(req, res, next){
   +'values (?,?,?,?,?)';
   
   var query = connection.query(sql, [title,url,expireDay,startDate,endDate], function(err, result){
+    if(err){
+      console.error(err);
+      //throw err; //처리가 필요함.
+      res.status(400).send('error'); // 에러처리에 대한 예비코드
+      return;
+    }
+
+    console.log(query.sql);
+    console.log("Number of records inserted: " + result.affectedRows);
+    res.status(200).send('success');
+  });
+});
+
+router.post('/image', function(req, res, next){
+
+  if (!req.files){
+    return res.status(400).send('No files were uploaded.');  
+  }
+
+  let title = req.body.title;
+  let uploadImage = req.files.uploadImage;
+  console.log(uploadImage.name);
+  let filePath = "upload_images/"+ Date.now() + '-'+ uploadImage.name;
+  let url = req.protocol + '://' + req.get('host')+"/"+filePath;
+  let expireDay = req.body.expireDay;
+  let startDate = req.body.startDate;
+  let endDate = req.body.endDate;
+
+  var sql = 'insert into campaign_info '
+  +'(title,url,ad_expire_day,start_date,end_date)'
+  +'values (?,?,?,?,?)';
+  connection.beginTransaction(function(err) {
+    if (err) {
+      console.error(err);
+      connection.rollback(function() {
+          //throw err; //처리가 필요함.
+          res.status(400).send('error'); // 에러처리에 대한 예비코드
+          return;
+      }); 
+    }
+
+    var query = connection.query(sql, [title,url,expireDay,startDate,endDate], function(err, result){
+      if(err){
+        console.error(err);
+        connection.rollback(function() {
+          //throw err; //처리가 필요함.
+          res.status(400).send('error'); // 에러처리에 대한 예비코드
+          return;
+        });
+      }
+
+      // Use the mv() method to place the file somewhere on your server 
+      uploadImage.mv(filePath, function(err) {
+        if (err){
+          console.error(err);
+          connection.rollback(function() {
+            //throw err; //처리가 필요함.
+            res.status(400).send('error'); // 에러처리에 대한 예비코드
+            return;
+          });
+        }
+        console.log('File Uploaded!');
+      });
+
+      console.log(query.sql);
+      console.log("Number of records inserted: " + result.affectedRows);
+      connection.commit(function(err){
+        if(err){
+          console.error(err);
+          connection.rollback(function() {
+            //throw err; //처리가 필요함.
+            res.status(400).send('error'); // 에러처리에 대한 예비코드
+            return;
+          });
+        }
+        console.log('Transaction Complete.');
+      });
+      res.status(200).send('success');
+    });
+  });
+});
+
+router.put('/:campaignid', function(req, res, next){
+  let campaignId = req.params.campaignid;
+  let title = req.body.title;
+  let url = req.body.url;
+  let expireDay = req.body.expireDay;
+  let startDate = req.body.startDate;
+  let endDate = req.body.endDate;
+
+  var sql = 'update campaign_info'
+  +' set title= ?,url=?,ad_expire_day=?,start_date=?,end_date=?'
+  +' where id = ?';
+  
+  var query = connection.query(sql, [title,url,expireDay,startDate,endDate,campaignId], function(err, result){
     if(err){
       console.error(err);
       //throw err; //처리가 필요함.
