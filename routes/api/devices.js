11d267b0129227435a45d4195318ebef8a45aaad
hyeongyun <hyeongyun@Jiui-MacBook-Pro.local>
2017-07-16 03:23:43 +0900

    주석 삭제.

--- routes/api/devices.js
+++ routes/api/devices.js
@@ -4,34 +4,6 @@ var router = express.Router();
 
 var dbModule = require('../../config/db.js');
 
-// var connection = mysql.createConnection({
-//   host: 'localhost',
-//   port: 3306,
-//   user: 'root',
-//   password: 'root',
-//   database: 'campaigndb'
-// });
-
-
-// router.get('/:appid/devices', function (req, res) {
-//   campaignsQuery = connection.query('select device_id from device_for_app where app_id=?', [req.params.appid], function (err, rows) {
-//     if (err) {
-//       console.error(err);
-//       res.status(400).json({
-//         'code': -1,
-//         'msg': 'query error',
-//         'result': err
-//       });
-//     } else {
-//       res.status(200).json({
-//         'code': 0,
-//         'msg': 'suc',
-//         'result': rows
-//       });
-//     }
-//   });
-// });
-
 router.get('/:appid/devices', function (req, res) {
   var getRows;
   dbModule.withConnection(dbModule.pool, function (connection, next) {
@@ -54,25 +26,6 @@ router.get('/:appid/devices', function (req, res) {
     }
   });
 });
-
-// router.get('/:appid/devices/:deviceid', function (req, res) {
-//   campaignsQuery = connection.query('select * from device_for_app where app_id=? and device_id=?', [req.params.appid, req.params.deviceid], function (err, rows) {
-//     if (err) {
-//       console.error(err);
-//       res.status(400).json({
-//         'code': -1,
-//         'msg': 'query error',
-//         'result': err
-//       });
-//     } else {
-//       res.status(200).json({
-//         'code': 0,
-//         'msg': 'suc',
-//         'result': rows
-//       });
-//     }
-//   });
-// });
 
 router.get('/:appid/devices/:deviceid', function (req, res) {
   var getRow;
@@ -97,24 +50,6 @@ router.get('/:appid/devices/:deviceid', function (req, res) {
   });
 });
 
-// router.post('/:appid/devices', function (req, res) {
-//   campaignsQuery = connection.query('insert into device_for_app (app_id, device_id) values (?, ?)', [req.params.appid, req.body.deviceid], function (err, rows) {
-//     if (err) {
-//       console.error(err);
-//       res.status(400).json({
-//         'code': -1,
-//         'msg': 'query error',
-//         'result': err
-//       });
-//     } else {
-//       res.status(200).json({
-//         'code': 0,
-//         'msg': 'suc'
-//       });
-//     }
-//   });
-// });
-
 router.post('/:appid/devices', function (req, res) {
   dbModule.inTransaction(dbModule.pool, function (connection, next) {
     connection.query('insert into device_for_app (app_id, device_id) values (?, ?)', [req.params.appid, req.body.deviceid], function (err, rows) {
@@ -136,32 +71,6 @@ router.post('/:appid/devices', function (req, res) {
     }
   });
 });
-
-// router.delete('/:appid/devices/:deviceid', function (req, res) {
-//   //todo: 배열을 안만들고 한다면?
-//   var devices = [];
-//   devices.push([req.params.appid, req.params.deviceid]);
-//   campaignsQuery = connection.query('delete from device_for_app where (app_id, device_id) in (?)', [devices], function (err, rows) {
-//     if (err) {
-//       console.error(err);
-//       res.status(400).json({
-//         'code': -1,
-//         'msg': 'query error',
-//         'result': err
-//       });
-//     } else if (!rows.affectedRows) {
-//       res.status(400).json({
-//         'code': -2,
-//         'msg': 'nothing changed'
-//       });
-//     } else {
-//       res.status(200).json({
-//         'code': 0,
-//         'msg': 'suc'
-//       });
-//     }
-//   });
-// });
 
 router.delete('/:appid/devices/:deviceid', function (req, res) {
   //todo: 배열을 안만들고 한다면?
