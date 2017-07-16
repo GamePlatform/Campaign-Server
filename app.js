af133d1bce82b232c2ce49280b1dbbb0335518ee
WJ <neodarkness728@gmail.com>
2017-07-16 10:21:21 +0900

    add effecti in app's modal

--- app.js
+++ app.js
@@ -13,11 +13,9 @@ var campaign = require('./routes/api/campaign');
 var appsApi = require('./routes/api/apps');
 var errorUtils = require('./error');
 
-//test js
+// test js
 var errorTest = require('./routes/api/errortest');
-
 var app = express();
-
 // view test
 var viewTest = require('./routes/viewtest');
 
@@ -26,7 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -50,15 +48,15 @@ app.use(errorUtils.logErrors);
 app.use(errorUtils.errorHandler);
 // basic error handler
 /*
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 */
 
 fs.mkdir("upload_images",function(e){
