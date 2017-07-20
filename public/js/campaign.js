4dbfb681168549764e5a954eb696c02dbfbe8357
hyeongyun <hyeongyun@Jiui-MBP.klounges.com>
2017-07-20 11:40:43 +0900

    타임피커 적용 및 form태그 ajax로 변경.

--- public/js/campaign.js
+++ public/js/campaign.js
@@ -27,19 +27,31 @@ $(document).ready(function () {
         hideExclude(urlType, $(this).val());
     });
 
+
+    $(".datepicker").val(getTimeStamp(new Date()));
+    $(".datepicker").datetimepicker({
+        format: 'yyyy-mm-dd hh:ii:ss'
+    });
+    // $(".datepicker").datepicker("option", "dateFormat", "yy-mm-dd");
+    // $(".datepicker").change(function (event) {
+    //     $(this).val(getTimeStamp($(this).val));
+    // });
+
+    /*
     $(function () {
         $(".datepicker").datepicker();
     });
+    */
 
     $(".ad_expire_day").click(function (event) {
-        $("#ad_expire_day").html($(this).context.innerHTML + '  <span class="caret"></span>');
+        $("#ad_expire_day").html($(this).context.innerHTML);
         $("#expireDay").val($(this).context.innerHTML);
     });
 
     $('#ad_expire_day_title').change(function (event) {
         var title = $(this).val();
         $('.ad_expire_day_title').each(function (index) {
-            $(this).html('<input type="checkbox">' + title);
+            $(this).html(title);
         }, this);
     });
 
@@ -48,6 +60,36 @@ $(document).ready(function () {
         $('.templateThumbnail').each(function (index) {
             $(this).attr("src", src);
         }, this);
+    });
+
+    $("#submit").click(function (event) {
+        var param = {
+            'template': $("input[name=template]:checked").val(),
+            'title': $("input[name=title]").val(),
+            'expireDay': $("input[name=expireDay]").val(),
+            'urlType': $("input[name=urlType]:checked").val(),
+            'url': $("input[name=url]").val(),
+            'startDate': $("input[name=startDate]").val(),
+            'endDate': $("input[name=endDate]").val(),
+        };
+        for (var key in param) {
+            if (!param[key].length) {
+                alert(key + '항목을 입력해주세요.');
+                return;
+            }
+        }
+        $.ajax({
+            type: "POST",
+            url: 'http://localhost:30022/api/campaigns/url',
+            data: param,
+            success: function (e) {
+                console.log(e);
+                window.location.reload();
+            },
+            error: function (e) {
+                alert(JSON.stringify(e));
+            }
+        });
     });
 });
 
@@ -58,4 +100,26 @@ function hideExclude(upperDiv, excludeId) {
     });
     // 파라미터로 넘겨 받은 id 요소는 show
     $("#" + upperDiv + excludeId).show();
+}
+
+function getTimeStamp(d) {
+    // var d = new Date();
+    var s =
+        leadingZeros(d.getFullYear(), 4) + '-' +
+        leadingZeros(d.getMonth() + 1, 2) + '-' +
+        leadingZeros(d.getDate(), 2) + ' ' +
+        leadingZeros(d.getHours(), 2) + ':' +
+        leadingZeros(d.getMinutes(), 2) + ':' +
+        leadingZeros(d.getSeconds(), 2);
+    return s;
+}
+
+function leadingZeros(n, digits) {
+    var zero = '';
+    n = n.toString();
+    if (n.length < digits) {
+        for (i = 0; i < digits - n.length; i++)
+            zero += '0';
+    }
+    return zero + n;
 }
