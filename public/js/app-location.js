f6b8b33663a0ce73bb138976cac3367cedaf00bf
donnaknew <donnaknew@gmail.com>
2017-07-22 17:40:05 +0900

    캠페인 모달

--- public/js/app-location.js
+++ public/js/app-location.js
@@ -13,6 +13,10 @@ $(document).ready(function(){
 	var locationModal = $('div#location_modal');
 	var campaignModal = $('div#campaign_modal');
 
+	var enrollCampaignList = $('#enroll-campaign-list');
+	var campaignList = $('#campaign-list');
+	// var campaignTitle = $('#campaign-title');
+
 	appAddBtn.on('click',function(e){
 		if(appModal.hasClass("hidden")){
 			appModal.show();
@@ -38,14 +42,50 @@ $(document).ready(function(){
 			method: "post",
 			success: function (result) {
 				appList.empty();
-				getAppInfo();		
+				getAppInfo();
 			},
 			error: function (e) {
 				console.log(JSON.stringify(e));
 			}
-		});	
-		appTitle.val('');	
+		});
+		appTitle.val('');
 	});
+
+	var campaignIds = []
+	campaignModal.on("click","a",function(e){
+		// console.log(selectedCampaign);
+		var selectCampaign = $(this);
+		var campaignId = selectCampaign.attr('name');
+
+		console.log(campaignId);
+		if(selectCampaign.hasClass('checked')){
+			selectCampaign.removeClass('checked');
+			campaignIds.splice(campaignId);
+		}else{
+			selectCampaign.addClass('checked');
+			campaignIds.push(campaignId);
+		}
+	});
+
+	// campaignModal.on("click","input[name='ok']",function(e){
+	// 	var campaign_info = []
+	// 	var campaignValue = appTitle.val();
+	// 	campaign_info.push({"title":appValue});
+	// 	$.ajax({
+	// 		url: 'http://localhost:30022/api/apps',
+	// 		contentType: "application/json",
+	// 		data: JSON.stringify({"app_info":app_info}),
+	// 		method: "post",
+	// 		success: function (result) {
+	// 			appList.empty();
+	// 			getAppInfo();
+	// 		},
+	// 		error: function (e) {
+	// 			console.log(JSON.stringify(e));
+	// 		}
+	// 	});
+	// 	appTitle.val('');
+	// });
 
 	$("input[name='cancel']").on("click",function(e){
 		appModal.hide();
@@ -76,6 +116,7 @@ $(document).ready(function(){
 
 	campaignAddBtn.on('click',function(e){
 		if(campaignModal.hasClass("hidden")){
+			getCampaignList();
 			campaignModal.show();
 			campaignModal.removeClass("hidden");
 		}else{
@@ -98,6 +139,23 @@ $(document).ready(function(){
 				var appDatas = result.apps;
 				for(var i=0;i<count;i++){
 					appList.append("<li><a name="+appDatas[i].id+">"+appDatas[i].title+"</a></li>");
+				}
+			},
+			error: function (e) {
+				console.log(JSON.stringify(e));
+			}
+		});
+	}
+
+	function getCampaignList(){
+		$.ajax({
+			type: "GET",
+			url: 'http://localhost:30022/api/campaigns',
+			success: function (result) {
+				var count = result.result.count;
+				var campaignsData = result.result.campaigns;
+				for(var i = 0; i < count; i++){
+					campaignList.append("<li><a name="+campaignsData[i].id+">"+campaignsData[i].title+"</a></li>");
 				}
 			},
 			error: function (e) {
