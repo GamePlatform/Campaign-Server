e861c3d2c46722e121c6e4d1052319077becc970
donnaknew <donnaknew@gmail.com>
2017-07-29 19:47:10 +0900

    ajax delete

--- public/js/app-location.js
+++ public/js/app-location.js
@@ -9,12 +9,30 @@ $(document).ready(function(){
 	var locationDelBtn = $('button#location-del-btn');
 	var campaignAddBtn = $('button#campaign-add-btn');
 	var campaignDelBtn = $('button#campaign-del-btn');
+
 	var appModal = $('div#app_modal');
 	var locationModal = $('div#location_modal');
 	var campaignModal = $('div#campaign_modal');
 
+	var appDelModal = $('div#app_del_modal');
+	var locationDelModal = $('div#location_del_modal');
+	var campaignDelModal = $('div#campaign_del_modal');
+
+	var appList = $('#app-list');
+	var appTitle = $('#app-title');
+	var locationList = $('#location-list');
+	var locationInputId = $('#location-id');
+	var locationDesc = $('#location-desc');
+	var campaignList = $('#campaign-list');
+
+	var appListDelModal = $('#del-modal-app-list');
+	var locationListDelModal = $('#del-modal-location-list');
+	var campaignListDelModal = $('#del-modal-campaign-list');
+
 	var modalCampaignList = $('#modal-campaign-list');
+
 	// var campaignTitle = $('#campaign-title');
+	var checkDelModal = 'delModal';
 
 	appAddBtn.on('click',function(e){
 		if(appModal.hasClass("hidden")){
@@ -25,11 +43,20 @@ $(document).ready(function(){
 			appModal.addClass("hidden");
 		}
 	});
+	appDelBtn.on('click',function(e){
+		if(appDelModal.hasClass("hidden")){
+			appListDelModal.empty();
+			getAppList(checkDelModal);
+			appDelModal.show();
+			appDelModal.removeClass("hidden");
+		}else{
+			appDelModal.hide();
+			appDelModal.addClass("hidden");
+		}
+	});
 	// init app-info
 	getAppList();
 
-	var appList = $('#app-list');
-	var appTitle = $('#app-title');
 	appModal.on("click","input[name='ok']",function(e){
 		var app_info = []
 		var appValue = appTitle.val();
@@ -49,6 +76,27 @@ $(document).ready(function(){
 		});
 		appTitle.val('');
 	});
+	appDelModal.on("click","input[name='ok']",function(e){
+		$.ajax({
+			url: '/api/apps',
+			contentType: "application/json",
+			data: JSON.stringify({"app_list": delAppsIdList}),
+			method: "DELETE",
+			success: function (result) {
+				appList.empty();
+				appListDelModal.empty();
+				appDelModal.hide();
+				appDelModal.addClass("hidden");
+				locationList.empty();
+				campaignList.empty();
+				getAppList();
+			},
+			error: function (e) {
+				console.log(JSON.stringify(e));
+			}
+		});
+		appTitle.val('');
+	});
 
 	var appId;
 	appList.on("click","a",function(e){
@@ -57,18 +105,25 @@ $(document).ready(function(){
 		campaignList.empty();
 		getLocationList(appId);
 	});
+	var delAppIdList = [];
+	appListDelModal.on("click","a",function(e){
+		delAppIdList.push({"id": $(this).attr("name")});
+	});
 
 	$("input[name='cancel']").on("click",function(e){
 		appModal.hide();
+		appDelModal.hide();
 		locationModal.hide();
+		locationDelModal.hide();
 		campaignModal.hide();
+		campaignDelModal.hide();
+
 		appModal.addClass("hidden");
+		appDelModal.addClass("hidden");
 		locationModal.addClass("hidden");
+		locationDelModal.addClass("hidden");
 		campaignModal.addClass("hidden");
-	});
-
-	appDelBtn.on('click',function(e){
-
+		campaignDelModal.addClass("hidden");
 	});
 
 	locationAddBtn.on('click',function(e){
@@ -80,10 +135,18 @@ $(document).ready(function(){
 			locationModal.addClass("hidden");
 		}
 	});
+	locationDelBtn.on('click',function(e){
+		if(locationDelModal.hasClass("hidden")){
+			locationListDelModal.empty();
+			getLocationList(appId, checkDelModal);
+			locationDelModal.show();
+			locationDelModal.removeClass("hidden");
+		}else{
+			locationDelModal.hide();
+			locationDelModal.addClass("hidden");
+		}
+	});
 
-	var locationList = $('#location-list');
-	var locationInputId = $('#location-id');
-	var locationDesc = $('#location-desc');
 	locationModal.on("click","input[name='ok']",function(e){
 		var locationInputIdValue = locationInputId.val();
 		var locationDescValue = locationDesc.val();
@@ -109,15 +172,43 @@ $(document).ready(function(){
 		locationDesc.val('');
 	});
 
+	var delLocationIdList = [];
+	locationDelModal.on("click","input[name='ok']",function(e){
+		$.ajax({
+			url: '/api/apps/'+appId+'/locations',
+			contentType: "application/json",
+			data: JSON.stringify({
+				"location_list": delLocationIdList
+			}),
+			method: "DELETE",
+			success: function (result) {
+				locationList.empty();
+				locationListDelModal.empty();
+				locationDelModal.hide();
+				locationDelModal.addClass("hidden");
+				campaignList.empty();
+				getLocationList(appId);
+			},
+			error: function (e) {
+				console.log(JSON.stringify(e));
+			}
+		});
+		locationInputId.val('');
+		locationDesc.val('');
+	});
+
 	var locationSelectIdValue;
+	var locationSeq;
 	locationList.on("click","a",function(e){
 		locationSelectIdValue = $(this).text();
+		locationSeq = $(this).attr("name");
 		campaignList.empty();
 		getCampaignListForLocation(appId,locationSelectIdValue);
 	});
 
-	locationDelBtn.on('click',function(e){
-
+	var delLocationIdList = [];
+	locationListDelModal.on("click","a",function(e){
+		delLocationIdList.push({"seq": $(this).attr("name")});
 	});
 
 
@@ -132,8 +223,17 @@ $(document).ready(function(){
 			campaignModal.addClass("hidden");
 		}
 	});
-
-	var campaignList = $('#campaign-list');
+	campaignDelBtn.on('click',function(e){
+		if(campaignModal.hasClass("hidden")){
+			campaignListDelModal.empty();
+			getCampaignListForLocation(appId, locationSelectIdValue, checkDelModal);
+			campaignDelModal.show();
+			campaignDelModal.removeClass("hidden");
+		}else{
+			campaignDelModal.hide();
+			campaignDelModal.addClass("hidden");
+		}
+	});
 
 	var campaignIds = []
 	campaignModal.on("click","a",function(e){
@@ -153,8 +253,11 @@ $(document).ready(function(){
 		}
 	});
 
-	campaignDelBtn.on('click',function(e){
-
+	var delCampaignIdList = [];
+	campaignDelModal.on("click","a",function(e){
+		var selectCampaign = $(this);
+		var campaignId = parseInt(selectCampaign.attr('name'));
+		delCampaignIdList.push({"campaign_id": campaignId});
 	});
 
 	campaignModal.on("click","input[name='ok']",function(e){
@@ -176,17 +279,45 @@ $(document).ready(function(){
 		campaignModal.hide();
 		campaignModal.addClass("hidden");
 	});
-	
 
-	function getAppList(){
+	campaignDelModal.on("click","input[name='ok']",function(e){
+		$.ajax({
+			url: '/api/apps/'+appId+'/locations/'
+				+locationSeq+'/campaigns',
+			contentType: "application/json",
+			data: JSON.stringify({"campaigns":delCampaignIdList}),
+			method: "DELETE",
+			success: function (result) {
+				campaignList.empty();
+				campaignListDelModal.empty();
+				campaignDelModal.hide();
+				campaignDelModal.addClass("hidden");
+				getCampaignListForLocation(appId,locationSelectIdValue);
+			},
+			error: function (e) {
+				console.log(JSON.stringify(e));
+			}
+		});
+		modalCampaignList.empty();
+		campaignModal.hide();
+		campaignModal.addClass("hidden");
+	});
+
+	function getAppList(checkDelModal){
 		$.ajax({
 			type: "GET",
 			url: '/api/apps',
 			success: function (result) {
 				var count=result.count;
 				var appDatas = result.apps;
-				for(var i=0;i<count;i++){
-					appList.append("<li><a name="+appDatas[i].id+">"+appDatas[i].title+"</a></li>");
+				if(checkDelModal === undefined){
+					for(var i=0;i<count;i++){
+						appList.append("<li><a name="+appDatas[i].id+">"+appDatas[i].title+"</a></li>");
+					}
+				}else if(checkDelModal === 'delModal'){
+					for(var i=0;i<count;i++){
+						appListDelModal.append("<li><a name="+appDatas[i].id+">"+appDatas[i].title+"</a></li>");
+					}
 				}
 			},
 			error: function (e) {
@@ -228,14 +359,20 @@ $(document).ready(function(){
 		});
 	}
 
-	function getLocationList(appId){
+	function getLocationList(appId, checkDelModal){
 		$.ajax({
 			type: "GET",
 			url: '/api/apps/'+appId+'/locations',
 			success: function (result) {
 				var locationDatas = result.result;
-				for(var i=0, length = locationDatas.length;i<length;i++){
-					locationList.append("<li><a name="+locationDatas[i].seq+">"+locationDatas[i].location_id+"</a></li>");
+				if(checkDelModal === undefined){
+					for(var i=0, length = locationDatas.length;i<length;i++){
+						locationList.append("<li><a name="+locationDatas[i].seq+">"+locationDatas[i].location_id+"</a></li>");
+					}
+				}else if(checkDelModal === 'delModal'){
+					for(var i=0, length = locationDatas.length;i<length;i++){
+						locationListDelModal.append("<li><a name="+locationDatas[i].seq+">"+locationDatas[i].location_id+"</a></li>");
+					}
 				}
 			},
 			error: function (e) {
@@ -244,7 +381,7 @@ $(document).ready(function(){
 		});
 	}
 
-	function getCampaignListForLocation(appId,locationId){
+	function getCampaignListForLocation(appId,locationId, checkDelModal){
 		$.ajax({
 			type: "GET",
 			url: '/api/apps/'+appId+'/locations/'+locationId+'/campaigns',
@@ -256,11 +393,16 @@ $(document).ready(function(){
 				});
 
 				campaignIds = [];
-				for(var i=0, length = campaignDatas.length;i<length;i++){
-					campaignList.append("<li><a>"+campaignDatas[i].camp_desc+"</a></li>");
-					campaignIds.push({"campaign_id":campaignDatas[i].campaign_id,"campaign_order":campaignDatas[i].campaign_order});
+				if(checkDelModal === undefined){
+					for(var i=0, length = campaignDatas.length;i<length;i++){
+						campaignList.append("<li><a>"+campaignDatas[i].camp_desc+"</a></li>");
+						campaignIds.push({"campaign_id":campaignDatas[i].campaign_id,"campaign_order":campaignDatas[i].campaign_order});
+					}
+				}else if(checkDelModal === 'delModal'){
+					for(var i=0, length = campaignDatas.length;i<length;i++){
+						campaignListDelModal.append("<li><a name="+campaignDatas[i].campaign_id+">"+campaignDatas[i].camp_desc+"</a></li>");
+					}
 				}
-
 			},
 			error: function (e) {
 				console.log(JSON.stringify(e));
