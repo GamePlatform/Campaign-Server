c28ef327a9ee38131b14a6fc4349eed3115d4a16
hyeongyun0916 <mhg5303@gmail.com>
2017-09-12 15:55:32 +0900

    파일이름 수정.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var campaign = require('./routes/api/campaign');
 var appsApi = require('./routes/api/apps');
 var locationsApi = require('./routes/api/locations');
 var devicesApi = require('./routes/api/devices');
-var clickApi = require('./routes/api/click');
+var reportApi = require('./routes/api/report');
 var errorUtils = require('./error');
 
 // test js
@@ -46,7 +46,7 @@ app.use(fileUpload());
 app.use('/', index);
 app.use('/api/users', usersApi);
 app.use('/api/campaigns', campaign);
-app.use('/api/campaigns', clickApi);
+app.use('/api/campaigns', reportApi);
 app.use('/api/apps', appsApi);
 app.use('/api/apps', locationsApi);
 app.use('/api/apps', devicesApi);
